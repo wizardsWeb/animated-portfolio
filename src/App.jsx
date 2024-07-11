df042a864ab10ce255eb8026aea3734f1078117c
Vinay Vora <122713139+wizardsWeb@users.noreply.github.com>
2024-07-11 11:21:25 +0530

    complete navbar with sidebar and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import React from 'react'
+import "./app.scss";
+import Navbar from './components/navbar/Navbar';
+import Test from './components/navbar/Test';
+
 const App = () => {
-  return <div>Hello World</div>;
-};
+  return (
+    <div>
+      <section id='Homepage'>
+        <Navbar />
+      </section>
+      <section id='Services'>Parallex</section>
+      <section>Services</section>
+      <section id='Portfolio'>Parallax</section>
+      <section>Portfolio 1</section>
+      <section>Portfolio 2</section>
+      <section>Portfolio 3</section>
+      <section id='Contact'>Contact</section>
+      {/* <section id='About'>About</section> */}
+      {/* <Test /> */}
+    </div>
+  )
+}
 
 export default App;
